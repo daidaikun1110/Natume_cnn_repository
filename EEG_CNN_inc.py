d4562b4ad02ac1877821a5c1e3581ae4b262b184
daidaikun1110 <daidaikun1110@gmail.com>
2026-04-16 07:59:22 +0000

    firast setup

---
+++ EEG_CNN_inc.py
@@ -0,0 +1,13 @@
+##CNN incremental learning
+#
+
+
+import numpy as np
+import csv
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.metrics import accuracy_score
+import tkinter as tk
+from tkinter import filedialog

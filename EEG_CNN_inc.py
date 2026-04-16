a543a8e0d008e1028d54825fc86d99d7639a15c5
daidaikun1110 <daidaikun1110@gmail.com>
2026-04-16 08:27:35 +0000

    fix devcontainer

--- EEG_CNN_inc.py
+++ EEG_CNN_inc.py
@@ -11,3 +11,31 @@ import torch.optim as optim
 from sklearn.metrics import accuracy_score
 import tkinter as tk
 from tkinter import filedialog
+
+# =========================
+# 設定
+# =========================
+
+# ==========================
+# subject data extraction
+# ==========================
+
+# ==========================
+# model definition
+# ==========================
+
+# ==========================
+# model incremental
+# ==========================
+
+# ==========================
+# main
+# ==========================
+
+# 学習
+
+# 評価
+
+# 結果表示
+
+# 結果保存
